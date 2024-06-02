37df3bd245f51cf18c443c4887406d9ed65bcc20
Connor Baker <connor.baker@tweag.io>
2024-06-02 23:12:17 +0000

    llama-cpp: CUDA fixups

--- pkgs/by-name/ll/llama-cpp/package.nix
+++ pkgs/by-name/ll/llama-cpp/package.nix
@@ -47,15 +47,8 @@ let
 
    cudaBuildInputs = with cudaPackages; [
     cuda_cccl.dev # <nv/target>
-
-    # A temporary hack for reducing the closure size, remove once cudaPackages
-    # have stopped using lndir: https://github.com/NixOS/nixpkgs/issues/271792
-    cuda_cudart.dev
-    cuda_cudart.lib
-    cuda_cudart.static
-    libcublas.dev
-    libcublas.lib
-    libcublas.static
+    cuda_cudart
+    libcublas
   ];
 
   rocmBuildInputs = with rocmPackages; [
